fd1235ff7244be19b3f612f28cc95edb4782b890
Brant Young <brant.young@gmail.com>
2013-04-14 22:34:59 +0800

    Gray proxy Username/Password label when controlled disabled

---
+++ sshproxy/CharmTextField.h
@@ -0,0 +1,13 @@
+//
+//  CharmTextField.h
+//  sshproxy
+//
+//  Created by Brant Young on 14/4/13.
+//  Copyright (c) 2013 Charm Studio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CharmTextField : NSTextField
+
+@end
---
+++ sshproxy/CharmTextField.m
@@ -0,0 +1,24 @@
+//
+//  CharmTextField.m
+//  sshproxy
+//
+//  Created by Brant Young on 14/4/13.
+//  Copyright (c) 2013 Charm Studio. All rights reserved.
+//
+
+#import "CharmTextField.h"
+
+@implementation CharmTextField
+
+- (void)setEnabled:(BOOL)flag
+{
+    [super setEnabled:flag];
+    
+    if (flag == NO) {
+        [self setTextColor:[NSColor disabledControlTextColor]];
+    } else {
+        [self setTextColor:[NSColor controlTextColor]];
+    }
+}
+
+@end
