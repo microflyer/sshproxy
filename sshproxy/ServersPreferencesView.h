27463bd3382382615c6174143e732ac1ae77ae93
Brant Young <brant.young@gmail.com>
2013-05-14 14:48:00 +0800

    Add new xib

---
+++ sshproxy/ServersPreferencesView.h
@@ -0,0 +1,13 @@
+//
+//  ServersPreferencesView.h
+//  sshproxy
+//
+//  Created by Brant Young on 14/5/13.
+//  Copyright (c) 2013 Charm Studio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ServersPreferencesView : NSView
+
+@end
